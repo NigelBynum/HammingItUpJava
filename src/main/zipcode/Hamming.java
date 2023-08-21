2b161128c9654c3175077e237dd2cef2e00c979e
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-08-21 14:22:45 -0400

    wrote code, can not figure out how to run test

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,26 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    private int Distance;
+    private String s;
+    private String s1;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException{
+this.s = s;
+this.s1 = s1
+
+        if(s.length!=s1.length){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        for (int i = 0; i < car2.length; i++) {
+            if (s.charAt[i] != s1.charAt[i]) {
+                Distance++;
+            }
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        return Distance;
     }
 }
